75ae7e7f5d760094590acb0265b5870b7a366227
Kyle Williams <williams.kyleg@gmail.com>
2023-11-20 08:44:50 -0500

    fulfilled: it must have a search view home page

---
+++ client/src/routes/root.jsx
@@ -0,0 +1,10 @@
+import Search from "../components/Search";
+
+export default function Root() {
+    return (
+        <>
+            <h1>Carnnovate</h1>
+            <Search />
+        </>
+    )
+}
