3c4970093ac15b9b4da4c6227cf84cec1d3d8ab6
Kyle Williams <williams.kyleg@gmail.com>
2024-04-04 07:20:58 -0400

    Skeleton logo, header, and footer added.

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -23,46 +23,51 @@ export default function Root() {
 
   return (
     <>
-      <nav>
-        <h1>Carnnovate</h1>
-        <ul>
-          <li>
-            <Link to={`/`}>Home</Link>
-          </li>
-          <li>
-            <Link to={`search`}>Search</Link>
-          </li>
-          {authorization === "authorized" ? (
-            <>
-              <li>
-                <Link to={`create`}>Create New Recipe</Link>
-              </li>
-              <li>
-                <Link to={`profile`}>Profile</Link>
-              </li>
-              <li>
-                <button type="button" onClick={() => handleClick()}>
-                  Logout
-                </button>
-              </li>
-            </>
-          ) : null}
-          {authorization === "authorizing" ||
-          authorization === "unauthorized" ? (
-            <>
-              <li>
-                <Link to={`register`}>Sign Up</Link>
-              </li>
-              <li>
-                <Link to={`login`}>Login</Link>
-              </li>
-            </>
-          ) : null}
-        </ul>
-      </nav>
-      <div>
+      <header>
+        <img src="../src/img/logo-150w.jpg" alt="Carnnovate logo" />
+        <nav>
+          <h1>Carnnovate</h1>
+          <ul>
+            <li>
+              <Link to={`/`}>Home</Link>
+            </li>
+            <li>
+              <Link to={`search`}>Search</Link>
+            </li>
+            {authorization === "authorized" ? (
+              <>
+                <li>
+                  <Link to={`create`}>Create New Recipe</Link>
+                </li>
+                <li>
+                  <Link to={`profile`}>Profile</Link>
+                </li>
+                <li>
+                  <button type="button" onClick={() => handleClick()}>
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : null}
+            {authorization === "authorizing" ||
+            authorization === "unauthorized" ? (
+              <>
+                <li>
+                  <Link to={`register`}>Sign Up</Link>
+                </li>
+                <li>
+                  <Link to={`login`}>Login</Link>
+                </li>
+              </>
+            ) : null}
+          </ul>
+        </nav>
+      </header>
+
+      <main>
         <Outlet context={[authorization, setAuthorization]} />
-      </div>
+      </main>
+      <footer>This is the footer.</footer>
     </>
   );
 }
