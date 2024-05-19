a485375a51952bf954c380549764f062f8a02a05
Kyle Williams <williams.kyleg@gmail.com>
2024-05-19 09:29:03 -0400

    Fixed syntax error for path to send index.html

--- server/server.js
+++ server/server.js
@@ -600,7 +600,7 @@ if (isProduction) {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
 
